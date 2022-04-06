0c430269482bf32bf9ff6e3393ecd9a09ac45169
mapl <placenti.marco@gmail.com>
2022-04-06 12:23:02 +0200

    db interactions, date and other stuff

--- drinkify/routes.py
+++ drinkify/routes.py
@@ -3,38 +3,30 @@ from flask import render_template, request, redirect, url_for
 from drinkify import db
 import time
 import pandas as pd
+import uuid
+
+
+#test = ((1, 'Water', 100, '1649070379.648753'), (1, 'Water', 100, '1649070426.912624'), (1, 'Water', 100, '1649070607.295744'), (1, 'Water', 100, '1649070647.2487102'), (1, 'Water', 100, '1649070702.4659996'), (1, 'Water', 100, '1649070723.6206126'), (1, 'Water', 100, '1649070834.7059758'), (1, 'Water', 100, '1649071127.0913403'))
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/insert', methods = ['post'])
-def insert():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        gender = request.form['optradio']
-        comment = request.form['comment']
-        db.insert_details(name,email,comment,gender)
-        return render_template('index.html')
-
-
-
-test = ((1, 'Water', 100, '1649070379.648753'), (1, 'Water', 100, '1649070426.912624'), (1, 'Water', 100, '1649070607.295744'), (1, 'Water', 100, '1649070647.2487102'), (1, 'Water', 100, '1649070702.4659996'), (1, 'Water', 100, '1649070723.6206126'), (1, 'Water', 100, '1649070834.7059758'), (1, 'Water', 100, '1649071127.0913403'))
+def main():
+    return redirect("/drink", code=302)
 
 @app.route('/drink', methods = ['get', 'post'])
 def drink():
     data = db.get_drinks()
     total = 0
     send_data = []
-    print(data)
+    #print(data)
     for row in data:
         d = {
-            "drink": row[1],
-            "amount": row[2],
-            "timestamp": time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(row[3])))
+            "drink": row[2],
+            "amount": row[3],
+            "water_amount": row[4],
+            "timestamp": row[5],
+            "location": row[6]
         }
-        total += int(row[2])
+        total += d['water_amount']
         send_data.append(d)
     
     goal = 2000
@@ -42,13 +34,20 @@ def drink():
 
     if request.method == 'POST':
         userID = 1
+        drink_id = uuidOne = uuid.uuid1()
         drink = request.form['drink']
         amount = request.form['amount']
-        timestamp = str(time.time()) 
-        db.insert_drink(userID, drink, amount, timestamp)
+        timestamp = request.form['timestamp']
+        location = request.form['location']
+        db.insert_drink(userID, drink_id, drink, amount, timestamp, location)
         return redirect(url_for('drink'))
     
     return render_template('drink.html', data=send_data, progress=progress, total=total) 
+
+@app.route('/clear')
+def clear_history():
+    db.delete_history()
+    return redirect("/drink", code=302)
     
 @app.route('/visuals')
 def visuals():
@@ -56,9 +55,10 @@ def visuals():
     df = []
     for row in data:
         d = {
-            "drink": row[1],
-            "amount": row[2],
-            "timestamp": time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(row[3])))
+            "drink": row[2],
+            "amount": row[3],
+            "timestamp": row[4],
+            "location": row[5]
         }
         df.append(d)
     df = pd.DataFrame(df)
