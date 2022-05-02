6db4c93fc01db433a42e14a2ad971d6b65a6979e
mapl <placenti.marco@gmail.com>
2022-05-02 22:10:06 +0200

    deployment ready

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from drinkify import app
 
 if __name__ == '__main__':
     # to run from mobile add host = 'IPv4 address' to the arguments 
-    app.run(debug = True)
+    app.run(debug=False, port='0.0.0.0')
